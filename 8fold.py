ddb71f6b6aefea706059f2f4e0dad0d9fa20dd86
jsjolund <j.sjolund@gmail.com>
2022-05-18 20:03:20 +0200

    Transform matrix

--- 8fold.py
+++ 8fold.py
@@ -1,51 +1,61 @@
 import svgwrite
 from copy import deepcopy
 from math import sqrt, cos, acos, sin, pi
+from matrix import Matrix3
 
 
 class Tile:
-    def __init__(self, name, vec):
+    def __init__(self, name, vec, tr=Matrix3()):
         self.vec = vec
         self.name = name
+        self.tr = tr
 
-    def flipX(self):
-        res = [(v[0], -v[1]) for v in self.vec]
-        return Tile(self.name, res)
-
-    def flipY(self):
-        res = [(-v[0], v[1]) for v in self.vec]
-        return Tile(self.name, res)
-
-    def rotCW(self):
-        res = [(-v[1], v[0]) for v in self.vec]
-        return Tile(self.name, res)
-
-    def rotCCW(self):
-        res = [(v[1], -v[0]) for v in self.vec]
-        return Tile(self.name, res)
-
-    def rot(self, t):
-        res = [(v[0]*cos(t)-v[1]*sin(t), v[0]*sin(t)+v[1]*cos(t))
-               for v in self.vec]
-        return Tile(self.name, res)
-
-    def scl(self, s):
-        res = [(v[0]*s, v[1]*s) for v in self.vec]
-        return Tile(self.name, res)
-
-    def tra(self, px, py=0):
+    def vtra(self, px, py=0):
         if type(px) is tuple:
             py = px[1]
             px = px[0]
         res = [(v[0]+px, v[1]+py) for v in self.vec]
         return Tile(self.name, res)
 
+    def transform(self):
+        mulVec = []
+        for v in self.vec:
+            x = v[0] * self.tr.val[0] + v[1] * self.tr.val[3] + self.tr.val[6]
+            y = v[0] * self.tr.val[1] + v[1] * self.tr.val[4] + self.tr.val[7]
+            mulVec.append((x, y))
+        return mulVec
+
+    def flipX(self):
+        self.tr.flipX()
+        return self
+
+    def flipY(self):
+        self.tr.flipY()
+        return self
+
+    def rot(self, t):
+        self.tr.rot(t)
+        return self
+
+    def scl(self, s):
+        self.tr.scl(s)
+        return self
+
+    def tra(self, x, y=0):
+        if type(x) is tuple:
+            y = x[1]
+            x = x[0]
+        self.tr.tra(x, y)
+        return self
+
+    def cpy(self):
+        return Tile(self.name, deepcopy(self.vec), self.tr.cpy())
+
     def __getitem__(self, key):
-        return deepcopy(self.vec[key])
+        return self.transform()[key]
 
 
 image = svgwrite.Drawing('test.svg', size=(600, 600))
-tile_scl = 100
 
 
 def draw(tiles):
@@ -58,9 +68,10 @@ def draw(tiles):
             case 'T2': color = 'blue'
             case 'T3': color = 'green'
             case 'T4': color = 'red'
-        path = tile.scl(tile_scl).vec
-        image.add(image.polygon(path, id=tile.name,
-                  stroke='black', fill=color, transform=f"translate(200,200)"))
+        path = tile.transform()
+        image.add(image.polygon(path, id=tile.name, stroke='black', stroke_width='0.01px',
+                                fill=color, transform="translate(200,200),scale(100)"))
+        # fill=color))
 
 
 dx = cos(pi/4)
@@ -75,62 +86,71 @@ theta = -acos((xa*xb+ya*yb)/(sqrt(xa**2+ya**2)*sqrt(xb**2+yb**2)))
 
 T1 = Tile('T1', [(dx+1, 0), (dx*2+1, dy), (dx*2+1, dy+1),
                  (dx+1, dy*2+1), (dx, dy*2+1), (0, dy+1), (0, dy), (dx, 0)])
-T1 = T1.tra(-dx-0.5, -dy-0.5)  # Mid point at origin
-T1r = T1.rot(theta)
+T1 = T1.vtra(-dx-0.5, -dy-0.5)  # Mid point at origin
+T1r = T1.cpy().rot(theta)
 T1scl = (T1r[4][1]-T1r[0][1])/(sqrt(2)+1+dx +
                                1+1+sqrt(2)+sqrt(1-(sqrt(2)/2)**2))
 
 T2 = Tile('T2', [(0, dy), (2, dy), (dx+2, 0)])
-T2 = T2.tra(0, -dy)  # Left bottom at origin
-T2r = T2.rot(-theta).flipX()
+T2 = T2.vtra(0, -dy)  # Left bottom at origin
+T2r = T2.cpy().rot(theta).flipX()
 T2scl = (T2r[0][1]-T2r[1][1])/(sqrt(1-(sqrt(2)/2)**2)*2)
 
 T3 = Tile('T3', [(0, 1), (1, 0), (1, 1)])
-T3 = T3.tra(0, -1)  # Left bottom at origin
+T3 = T3.vtra(0, -1)  # Left bottom at origin
 T3scl = 1/(1+sqrt(2)+1+1+1+sqrt(2)+1)
 
 T4 = Tile('T4', [(0, dy), (dx, 0), (dx+1, 0), (1, dy)])
-T4 = T4.tra(0, -dy)  # Left bottom at origin
+T4 = T4.vtra(0, -dy)  # Left bottom at origin
 T4scl = dy/((1+dy*2)*2+dy)
 
-t1 = T1.scl(T1scl)
-t2 = T2.scl(T1scl)
-t3 = T3.scl(T1scl)
-t4 = T4.scl(T1scl)
+# draw(T1)
+# draw(T1r)
+# draw(T2)
+# draw(T2r)
+# draw(T3)
+# draw(T4)
+
+# print(T1scl)
+# print(T2scl)
+# print(T3scl)
+# print(T4scl)
+
 T1s = []
-T1s.append(t2.tra(T1r[7]))
-T1s.append(t3.rot(pi/4).rotCW().tra(T1r[0]))
-T1s.append(t4.rotCW().tra(T1s[-1][2]))
-T1s.append(t4.flipY().tra(T1s[-1][2]))
-T1s.append(t4.flipY().tra(T1s[-1][3]))
-T1s.append(t4.flipY().flipX().tra(T1s[-1][0]))
-T1s.append(t3.rot(pi/4).flipY().flipX().tra(T1s[-1][3]))
-T1s.append(t3.rot(pi/4).flipY().tra(T1s[-1][0]))
-T1s.append(t3.rot(pi/4).flipY().flipX().tra(T1s[-1][2]))
-T1s.append(t2.rot(-pi/4).tra(T1s[-1][1]))
-T1s.append(t3.flipX().tra(T1s[-1][2]))
-T1s.append(t3.flipY().tra(T1s[-1][1]))
-T1s.append(t3.rotCCW().tra(T1s[3][1]))
-T1s.append(t3.rotCW().tra(T1s[-1][1]))
-T1s.extend([t.rot(pi/2) for t in T1s])
-T1s.extend([t.rot(pi) for t in T1s])
-T1s.append(t1)
+T1s.append(T2.cpy().tra(T1r[7]).scl(T1scl))
+T1s.append(T3.cpy().tra(T1r[0]).scl(T1scl).rot(3*pi/4))
+T1s.append(T4.cpy().tra(T1s[-1][2]).scl(T1scl).rot(pi/2))
+T1s.append(T4.cpy().tra(T1s[-1][2]).scl(T1scl).flipY())
+T1s.append(T4.cpy().tra(T1s[-1][3]).scl(T1scl).flipY())
+T1s.append(T4.cpy().tra(T1s[-1][0]).scl(T1scl).flipY().flipX())
+T1s.append(T3.cpy().tra(T1s[-1][3]).scl(T1scl).flipY().flipX().rot(pi/4))
+T1s.append(T3.cpy().tra(T1s[-1][0]).scl(T1scl).flipY().rot(pi/4))
+T1s.append(T3.cpy().tra(T1s[-1][2]).scl(T1scl).flipY().flipX().rot(pi/4))
+T1s.append(T2.cpy().tra(T1s[-1][1]).scl(T1scl).rot(-pi/4))
+T1s.append(T3.cpy().tra(T1s[-1][2]).scl(T1scl).flipX())
+T1s.append(T3.cpy().tra(T1s[-1][1]).scl(T1scl).flipY())
+T1s.append(T3.cpy().tra(T1s[3][1]).scl(T1scl).rot(-pi/2))
+T1s.append(T3.cpy().tra(T1s[-1][1]).scl(T1scl).rot(pi/2))
+# T1s.extend([t.cpy().rot(pi/2) for t in T1s])
+# T1s.extend([t.rot(pi) for t in T1s])
+# T1s.append(t1)
 # T1s = [t.rot(-theta) for t in T1s]
+draw(T1r)
 draw(T1s)
 
-# draw(T2r)
-# draw(T2.scl(T2scl))
-# draw(T3.scl(T2scl))
+# # draw(T2r)
+# # draw(T2.scl(T2scl))
+# # draw(T3.scl(T2scl))
 
-# draw(T3)
-# draw(T1.scl(T3scl))
-# draw(T3.scl(T3scl))
-# draw(T4.scl(T3scl))
+# # draw(T3)
+# # draw(T1.scl(T3scl))
+# # draw(T3.scl(T3scl))
+# # draw(T4.scl(T3scl))
 
-# draw(T4)
-# draw(T1.scl(T4scl))
-# draw(T3.scl(T4scl))
-# draw(T4.scl(T4scl))
+# # draw(T4)
+# # draw(T1.scl(T4scl))
+# # draw(T3.scl(T4scl))
+# # draw(T4.scl(T4scl))
 
 
 image.save()
---
+++ matrix.py
@@ -0,0 +1,113 @@
+import math
+from copy import deepcopy
+
+M00 = 0
+M01 = 3
+M02 = 6
+M10 = 1
+M11 = 4
+M12 = 7
+M20 = 2
+M21 = 5
+M22 = 8
+
+# Column major matrix
+class Matrix3:
+    def __repr__(self):
+        return str(self.val)
+        
+    def __init__(self):
+        self.val = [0]*9
+        self.tmp = [0]*9
+        self.idt()
+
+    def cpy(self):
+        m = Matrix3()
+        m.val = deepcopy(self.val)
+        m.tmp = deepcopy(self.tmp)
+        return m
+
+    def flipX(self):
+        self.val[M11] = -self.val[M11]
+
+    def flipY(self):
+        self.val[M00] = -self.val[M00]
+
+    def idt(self):
+        self.val[M00] = 1
+        self.val[M10] = 0
+        self.val[M20] = 0
+        self.val[M01] = 0
+        self.val[M11] = 1
+        self.val[M21] = 0
+        self.val[M02] = 0
+        self.val[M12] = 0
+        self.val[M22] = 1
+        return self
+
+    def mul(self, m):
+        val = self.val
+
+        v00 = val[M00] * m[M00] + val[M01] * m[M10] + val[M02] * m[M20]
+        v01 = val[M00] * m[M01] + val[M01] * m[M11] + val[M02] * m[M21]
+        v02 = val[M00] * m[M02] + val[M01] * m[M12] + val[M02] * m[M22]
+
+        v10 = val[M10] * m[M00] + val[M11] * m[M10] + val[M12] * m[M20]
+        v11 = val[M10] * m[M01] + val[M11] * m[M11] + val[M12] * m[M21]
+        v12 = val[M10] * m[M02] + val[M11] * m[M12] + val[M12] * m[M22]
+
+        v20 = val[M20] * m[M00] + val[M21] * m[M10] + val[M22] * m[M20]
+        v21 = val[M20] * m[M01] + val[M21] * m[M11] + val[M22] * m[M21]
+        v22 = val[M20] * m[M02] + val[M21] * m[M12] + val[M22] * m[M22]
+
+        val[M00] = v00
+        val[M10] = v10
+        val[M20] = v20
+        val[M01] = v01
+        val[M11] = v11
+        val[M21] = v21
+        val[M02] = v02
+        val[M12] = v12
+        val[M22] = v22
+        return self
+
+
+    def tra(self, x, y):
+        self.tmp[M00] = 1
+        self.tmp[M10] = 0
+        self.tmp[M20] = 0
+
+        self.tmp[M01] = 0
+        self.tmp[M11] = 1
+        self.tmp[M21] = 0
+
+        self.tmp[M02] = x
+        self.tmp[M12] = y
+        self.tmp[M22] = 1
+        self.mul(self.tmp)
+        return self
+    
+    def scl(self, s):
+        self.val[M00] *= s
+        self.val[M11] *= s
+        return self
+    
+    def rot(self, t):
+        if t == 0:
+            return self
+        cos = math.cos(t)
+        sin = math.sin(t)
+
+        self.tmp[M00] = cos
+        self.tmp[M10] = sin
+        self.tmp[M20] = 0
+
+        self.tmp[M01] = -sin
+        self.tmp[M11] = cos
+        self.tmp[M21] = 0
+
+        self.tmp[M02] = 0
+        self.tmp[M12] = 0
+        self.tmp[M22] = 1
+        self.mul(self.tmp)
+        return self
